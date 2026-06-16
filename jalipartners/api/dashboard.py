ce024efa242897d1b4609eb1ee4ce405554e21f5
desiremukunzi <desiremukunzi@gmail.com>
2026-06-16 16:41:37 +0200

    filter company on home page

--- jalipartners/api/dashboard.py
+++ jalipartners/api/dashboard.py
@@ -1,9 +1,6 @@
 import frappe
 
 @frappe.whitelist()
-def set_dashboard_company(company=None):
-    if company:
-        frappe.defaults.set_user_default("jali_dashboard_company", company)
-    else:
-        frappe.defaults.clear_user_default("jali_dashboard_company")
-    return company or ""
+def set_dashboard_company(company):
+    frappe.defaults.set_user_default("Company", company)
+    return company
